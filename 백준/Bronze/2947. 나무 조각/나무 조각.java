98f845bd368e3d2770d40d8b564e0c35f785f095
이종하 <90444513+Jonghai@users.noreply.github.com>
2025-05-24 23:42:20 +0900

    [Bronze I] Title: 나무 조각, Time: 120 ms, Memory: 15956 KB -BaekjoonHub

---
+++ 백준/Bronze/2947. 나무 조각/나무 조각.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] line = reader.readLine().split(" ");
+        int[] numbers = new int[5];
+        for (int i = 0; i  < line.length; i++) {
+            numbers[i] = Integer.parseInt(line[i]);
+        }
+
+        while (true) {
+            boolean flag = false;
+            for (int i = 0; i < numbers.length - 1; i++) {
+                int firstNum = numbers[i];
+                int secondNum = numbers[i + 1];
+                if (firstNum > secondNum) {
+                    numbers[i] = secondNum;
+                    numbers[i + 1] = firstNum;
+                    printNumbers(numbers);
+                    flag = true;
+                }
+            }
+            if (!flag) {
+                break;
+            }
+        }
+    }
+
+
+    static void printNumbers(int[] numbers) {
+        Arrays.stream(numbers).forEach(n -> System.out.print(n + " "));
+        System.out.println();
+    }
+}
