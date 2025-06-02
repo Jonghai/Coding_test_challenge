04ac7af4a879b6d74e0bee282f80ed0fc70c517c
이종하 <90444513+Jonghai@users.noreply.github.com>
2025-06-02 23:13:07 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1668 ms, Memory: 332404 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Main {
+    static List<Integer>[] graph;
+    static boolean[] visited;
+    static int N, M;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        // 그래프 초기화
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // 간선 입력
+        for (int i = 0; i < M; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        visited = new boolean[N + 1];
+        int count = 0;
+
+        // 모든 정점을 순회
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    static void dfs(int node) {
+        visited[node] = true;
+        for (int next : graph[node]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+}
